1f3ac88d210a3fff066441e2ada9efe4b641d6b4
Jason Walzak <walzak@gmail.com>
2017-07-10 02:07:08 -0400

    JavaScript sorts low to high More text in the .txt file

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 let progressBar = [];
-const bar = 3;
+const bar = 10;
 for (let i = 0; i < bar; i++) {
   progressBar[i] = Math.floor((Math.random() * 10000) + 1);
 }
@@ -9,16 +9,19 @@ console.log(progressBar);
 let sorted = [];
 
 let sortArray = function(arr){
-  let biggest = 0;
-  for(let i = 0; i < progressBar.length-1; i++) {
-    if(progressBar[i] > progressBar[i + 1]) {
-      let temp = progressBar[i];
-      progressBar[i] = progressBar[i + 1];
-      progressBar[i + 1] = temp;
+  let swapped;
+  do {
+    swapped = false;
+    for(let i = 0; i < progressBar.length - 1; i++) {
+      if(progressBar[i] > progressBar[i + 1]) {
+        let temp = progressBar[i];
+        progressBar[i] = progressBar[i + 1];
+        progressBar[i + 1] = temp;
+        swapped = true;
+      }
     }
-  }
+  } while(swapped);
   console.log(progressBar);
 }
 
 sortArray(progressBar);
-
