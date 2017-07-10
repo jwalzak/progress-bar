79adf4269d1682f6aa4711737b999c9180b2974f
Jason Walzak <walzak@gmail.com>
2017-07-10 01:26:19 -0400

    Added some more text. Started working on the sort function. Added the JavaScript file to the HTML.

--- script/main.js
+++ script/main.js
@@ -1,7 +1,24 @@
 let progressBar = [];
-let bar = 3;
-for (let i = 0; i < bar; i++){
+const bar = 3;
+for (let i = 0; i < bar; i++) {
   progressBar[i] = Math.floor((Math.random() * 10000) + 1);
 }
 
-console.log(progressBar[0], progressBar[1], progressBar[2]);
+console.log(progressBar);
+
+let sorted = [];
+
+let sortArray = function(arr){
+  let biggest = 0;
+  for(let i = 0; i < progressBar.length-1; i++) {
+    if(progressBar[i] > progressBar[i + 1]) {
+      let temp = progressBar[i];
+      progressBar[i] = progressBar[i + 1];
+      progressBar[i + 1] = temp;
+    }
+  }
+  console.log(progressBar);
+}
+
+sortArray(progressBar);
+
