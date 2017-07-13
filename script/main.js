866c6b6f9d9b7369bd2b66e5b16a5037b63d83d5
Jason Walzak <walzak@gmail.com>
2017-07-13 19:55:31 -0400

    Created a function that will will display each item in the array as a div in the browser. Each div is given a width that is the same as it's percnet value against the largest number in the array. The top div will always have a width of 100% This works with arrays of any size.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 let progressBar = [];
-const bar = 10;
+const bar = 3;
 for (let i = 0; i < bar; i++) {
   progressBar[i] = Math.floor((Math.random() * 10000) + 1);
 }
@@ -29,14 +29,35 @@ sortArray(progressBar);
 sorted = sorted.reverse();
 console.log(sorted);
 
-//Calculate the % of each element in the array compared against the largest number array[0];
 let percentArray = [];
+
+//Calculate the % of each element in the array compared against the largest number array[0];
 function calcPerc(arr) {
   let max = 100;
   let bigVal = arr[0]; //The largest value in the array
   for(let i = 0; i < arr.length; i++) {
-    percentArray[i] = (arr[i] / bigVal) * max;
+    percentArray[i] = Math.floor((arr[i] / bigVal) * max);
   }
   console.log(percentArray);
   // return(precentArray);
 }
+
+calcPerc(sorted);
+
+function placeElement(arr) {
+    for(let i = 0; i < arr.length; i++) {
+      let color = Math.floor((Math.random() * 999999) + 1);
+      let el = document.createElement('div');
+      let t = document.createTextNode(arr[i] + '%');
+      el.setAttribute('class', 'bar');
+      el.style.background = '#' + color;
+      el.style.width = arr[i] + '%';
+      el.style.height = '50px';
+      el.style.textAlign = 'center';
+      el.appendChild(t);
+      let divEl = document.querySelector('.progress--bar');
+      divEl.appendChild(el);
+    }
+}
+
+placeElement(percentArray);
